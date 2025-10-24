b64ac527f25a0839d7f2875abf4bd6e012547f38
Jaemin0826 <shiibadog@gmail.com>
2025-10-24 11:06:52 +0900

    Updates

--- assets/index-CGJnYigT.js
+++ assets/index-CGJnYigT.js
@@ -3538,7 +3538,7 @@ ${this.customData.serverResponse}`:this.message=this._baseMessage}}var Pi;(funct
     color: var(--Gray-White, #fff);
     font-family: Jalnan;
     font-size: 64px;
-    font-weight: 700;
+    font-weight: 400; /* Jalnan 제공 가중치에 맞춰 faux-bold 방지 */
     letter-spacing: -1.28px;
   }
 
